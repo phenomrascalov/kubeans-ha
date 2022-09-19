7a19c617c99d786ff80bfd476ac4120116fbc374
tekton <tekton@kalkinma.com.tr>
2022-09-20 02:45:28 +0300

    Update

--- install_etcd_master1.sh
+++ install_etcd_master1.sh
@@ -10,24 +10,7 @@ Documentation=https://github.com/coreos
 
 
 [Service]
-ExecStart=/usr/local/bin/etcd \
-  --name #master1 \
-  --cert-file=/etc/etcd/kubernetes.pem \
-  --key-file=/etc/etcd/kubernetes-key.pem \
-  --peer-cert-file=/etc/etcd/kubernetes.pem \
-  --peer-key-file=/etc/etcd/kubernetes-key.pem \
-  --trusted-ca-file=/etc/etcd/ca.pem \
-  --peer-trusted-ca-file=/etc/etcd/ca.pem \
-  --peer-client-cert-auth \
-  --client-cert-auth \
-  --initial-advertise-peer-urls https://#master1:2380 \
-  --listen-peer-urls https://#master1:2380 \
-  --listen-client-urls https://#master1:2379,http://127.0.0.1:2379 \
-  --advertise-client-urls https://#master1:2379 \
-  --initial-cluster-token etcd-cluster-0 \
-  --initial-cluster #master1=https://#master1:2380,#master2=https://#master2:2380,#master3=https://#master3:2380 \
-  --initial-cluster-state new \
-  --data-dir=/var/lib/etcd
+ExecStart=/usr/local/bin/etcd --name #master1 --cert-file=/etc/etcd/kubernetes.pem --key-file=/etc/etcd/kubernetes-key.pem --peer-cert-file=/etc/etcd/kubernetes.pem --peer-key-file=/etc/etcd/kubernetes-key.pem --trusted-ca-file=/etc/etcd/ca.pem --peer-trusted-ca-file=/etc/etcd/ca.pem --peer-client-cert-auth --client-cert-auth --initial-advertise-peer-urls https://#master1:2380 --listen-peer-urls https://#master1:2380 --listen-client-urls https://#master1:2379,http://127.0.0.1:2379 --advertise-client-urls https://#master1:2379 --initial-cluster-token etcd-cluster-0 --initial-cluster #master1=https://#master1:2380,#master2=https://#master2:2380,#master3=https://#master3:2380 --initial-cluster-state new --data-dir=/var/lib/etcd
 Restart=on-failure
 RestartSec=5
 
