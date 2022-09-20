ebe310c785930efa822792c22a46bd578a6bfd69
phenomrascalov <62064577+phenomrascalov@users.noreply.github.com>
2022-09-20 03:53:39 +0300

    Update init_etcd.sh

--- init_etcd.sh
+++ init_etcd.sh
@@ -2,4 +2,4 @@ while read var ip; do
         sed -i "s/$var/$ip/" install_etcd_master1.sh
         sed -i "s/$var/$ip/" install_etcd_master2.sh
         sed -i "s/$var/$ip/" install_etcd_master3.sh
-done <<< "servers"
+done < "servers"
