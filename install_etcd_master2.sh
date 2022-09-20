6269f7eece5e0aceba7e829288bf2918653e6e10
tekton <tekton@kalkinma.com.tr>
2022-09-20 12:19:18 +0300

    Update

--- install_etcd_master2.sh
+++ install_etcd_master2.sh
@@ -1,5 +1,5 @@
 sudo mkdir /etc/etcd /var/lib/etcd
-sudo mv ~/ca.pem ~/kubernetes.pem ~/kubernetes-key.pem /etc/etcd
+sudo mv /root/ca.pem /root/kubernetes.pem /root/kubernetes-key.pem /etc/etcd
 wget https://github.com/etcd-io/etcd/releases/download/v3.5.5/etcd-v3.5.5-linux-amd64.tar.gz
 tar xvzf etcd-v3.5.5-linux-amd64.tar.gz
 sudo mv etcd-v3.5.5-linux-amd64/etcd* /usr/local/bin/
@@ -10,7 +10,24 @@ Documentation=https://github.com/coreos
 
 
 [Service]
-ExecStart=/usr/local/bin/etcd --name #master2# --cert-file=/etc/etcd/kubernetes.pem --key-file=/etc/etcd/kubernetes-key.pem --peer-cert-file=/etc/etcd/kubernetes.pem --peer-key-file=/etc/etcd/kubernetes-key.pem --trusted-ca-file=/etc/etcd/ca.pem --peer-trusted-ca-file=/etc/etcd/ca.pem --peer-client-cert-auth --client-cert-auth --initial-advertise-peer-urls https://#master2#:2380 --listen-peer-urls https://#master2#:2380 --listen-client-urls https://#master2#:2379,http://127.0.0.1:2379 --advertise-client-urls https://#master2#:2379 --initial-cluster-token etcd-cluster-0 --initial-cluster #master1#=https://#master1#:2380,#master2#=https://#master2#:2380,#master3#=https://#master3#:2380 --initial-cluster-state new --data-dir=/var/lib/etcd
+ExecStart=/usr/local/bin/etcd \
+  --name #master2# \
+  --cert-file=/etc/etcd/kubernetes.pem \
+  --key-file=/etc/etcd/kubernetes-key.pem \
+  --peer-cert-file=/etc/etcd/kubernetes.pem \
+  --peer-key-file=/etc/etcd/kubernetes-key.pem \
+  --trusted-ca-file=/etc/etcd/ca.pem \
+  --peer-trusted-ca-file=/etc/etcd/ca.pem \
+  --peer-client-cert-auth \
+  --client-cert-auth \
+  --initial-advertise-peer-urls https://#master2#:2380 \
+  --listen-peer-urls https://#master2#:2380 \
+  --listen-client-urls https://#master2#:2379,http://127.0.0.1:2379 \
+  --advertise-client-urls https://#master2#:2379 \
+  --initial-cluster-token etcd-cluster-0 \
+  --initial-cluster #master1#=https://#master1#:2380,#master2#=https://#master2#:2380,#master3#=https://#master3#:2380 \
+  --initial-cluster-state new \
+  --data-dir=/var/lib/etcd
 Restart=on-failure
 RestartSec=5
 
