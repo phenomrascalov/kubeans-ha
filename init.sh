b21b91ca815c7cc0ea34113c4f59c90b0d8d74d4
tekton <tekton@kalkinma.com.tr>
2022-09-20 11:15:21 +0300

    Update

--- init.sh
+++ init.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
 while read var ip; do
         sed -i "s/$var/$ip/" install.sh
+<<<<<<< HEAD
+done << "servers"
+=======
 done < "servers"
+>>>>>>> ebe310c785930efa822792c22a46bd578a6bfd69
--- init_etcd.sh
+++ init_etcd.sh
@@ -1,5 +1,10 @@
+#!/bin/bash
 while read var ip; do
         sed -i "s/$var/$ip/" install_etcd_master1.sh
         sed -i "s/$var/$ip/" install_etcd_master2.sh
         sed -i "s/$var/$ip/" install_etcd_master3.sh
+<<<<<<< HEAD
+done << "servers"
+=======
 done < "servers"
+>>>>>>> ebe310c785930efa822792c22a46bd578a6bfd69
