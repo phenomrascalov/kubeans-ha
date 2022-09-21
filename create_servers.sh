518be821c2ada95e0746b0f47893b2e81f2bd054
tekton <tekton@kalkinma.com.tr>
2022-09-21 10:34:57 +0300

    Update

--- create_servers.sh
+++ create_servers.sh
@@ -1,10 +1,13 @@
 while read list; do
+if [ ! -z "$list" ]
+then
 
         ip=$(ansible-inventory -i inventory.yaml --list | jq -r ".$list.hosts[]")
 
         list="#$list#"
 
         echo $list $ip >> servers
+fi
 
 done < list
 echo "" >> servers
