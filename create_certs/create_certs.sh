c15178f468dbe47b5f7bc7e307f643de2a5564e9
tekton <tekton@kalkinma.com.tr>
2022-09-21 15:57:20 +0300

    Update

--- create_certs/create_certs.sh
+++ create_certs/create_certs.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-wget https://pkg.cfssl.org/R1.2/cfssl_linux-amd64
-wget https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64
-chmod +x cfssl*
-sudo mv cfssl_linux-amd64 /usr/local/bin/cfssl
-sudo mv cfssljson_linux-amd64 /usr/local/bin/cfssljson
 
 cfssl gencert -initca ca-csr.json | cfssljson -bare ca
 
